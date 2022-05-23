ec9e9803e5aa42e670e9add4072f0ca2330ef3b9
Eva <20bcs043@nith.ac.in>
2022-05-23 23:03:49 +0530

    production ready

--- face_rec/settings.py
+++ face_rec/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-v1so5!e%pyfsj33)(24dffqj5-bxdhx8lxq+yx$ig+k0i^e-9v'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
--- face_rec/urls.py
+++ face_rec/urls.py
@@ -15,13 +15,18 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from django.conf.urls import include
+from django.conf.urls import include, url
 from django.conf import settings
 from django.conf.urls.static import static
+
+from django.views.static import serve
 
 urlpatterns = [
     path('', include('app1.urls')),
     path('admin/', admin.site.urls),
+
+    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL,
